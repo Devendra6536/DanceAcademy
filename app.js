f68fe254ceb2106d7d69ed2b009a92c8f5d27906
Devendra kumar <devendrapal8682@gmail.com>
2022-07-04 13:09:08 +0530

    Service or about us updated

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+var mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+mongoose.connect('mongodb://localhost/contactDance', { useNewUrlParser: true });
 const port = 8000;
+
+// creating the schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+
 
 
 app.use('/static', express.static('static'));
@@ -28,11 +44,25 @@ app.get("/service", (req, res)=>{
     const param = { };
     res.status(200).render("service.pug",param);
 });
+app.get("/class", (req, res)=>{ 
+    const param = { };
+    res.status(200).render("contactAlert.pug",param);
+});
 app.get("/contact", (req, res)=>{ 
     const param = { };
     res.status(200).render("contact.pug",param);
 });
 
+app.post("/contact", (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("item was not saved to the databse")
+    })
+    res.status(200).render("contactAlert.pug");
+    // res.end("<h1>404 Not Found</h1> ");
+});
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
 });
